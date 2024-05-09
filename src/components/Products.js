d57b089f6adb4c6f7cf81d6929eb3c92d22cf4b8
VenusBridget <venusbri27@gmail.com>
2024-05-09 23:55:03 +0300

    Done

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,18 +1,24 @@
 import "./Products.css"
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import Search from "./Search";
 
 function Products({ handleAddProduct, productItems }) {
     const [category, setCategory] = useState('all');
+    const [searchInput, setSearchInput] = useState('');
 
     const handleCategoryFilter = (category) => {
         setCategory(category);
     };
 
     // const filteredProducts = category === 'all' ? data.productItems : data.productItems.filter(item => item.category === category);
-    const filteredProducts = category === 'all' ? productItems : productItems.filter(item => item.category === category);
+    const filteredProducts = productItems.filter(item => {
+        return(category === 'all' || item.category === category) &&
+        (item.brand.toLowerCase().includes(searchInput.toLowerCase()))
+    })
     return (
         <div>
+            <Search searchInput={searchInput} setSearchInput={setSearchInput}/>
             <div>
                 <button className='category-btn' onClick={() => handleCategoryFilter('all')}>All</button>
                 <button className='category-btn' onClick={() => handleCategoryFilter('child')}>Children</button>
@@ -27,6 +33,7 @@ function Products({ handleAddProduct, productItems }) {
                         <div>
                             <h3 className='prod-name'>{productitem.name}</h3>
                         </div>
+                        <p className="brand">Brand: {productitem.brand}</p>
                         <div className='price'>$ {productitem.price}</div>
                         <div>
                         <Link to={`/product/${productitem.id}`} className="btn btn-primary">Details</Link>

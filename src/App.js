f806874629aa767cd28718d1249d484a26c8a0a3
dudley-hue <dudleykimani331@gmail.com>
2024-05-09 15:38:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,76 @@
-
 import './App.css';
 import data from './Data';
 import Header from './components/Header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Products from './components/Products';
 import Cart from './components/Cart';
+import Review from './components/Review'; // Assuming this is your Review component
 import { useState } from 'react';
+import reviewData from './components/Reviewdata';
 
 function App() {
-  const {productItems} = data;
-  // initializing the array to empty
+  const { productItems } = data;
   const [cartItems, setCartItems] = useState([]);
 
-  const handleAddProduct = (product) =>{
-    const productExist = cartItems.find((item)=> item.id === product.id);
-    if(productExist){
-      setCartItems(cartItems.map((item)=> item.id === product.id ?
-    {...productExist, quantity: productExist.quantity + 1} : item))
-    }
-    else{
-      setCartItems([...cartItems, {...product, quantity: 1}])
-    }
-  }
-
-  const handleRemoveProduct = (product) =>{
-    const productExist = cartItems.find((item)=> item.id === product.id);
-    if(productExist.quantity === 1){
-      setCartItems(cartItems.filter((item)=> item.id !== product.id));
-    }
-    else{
+  const handleAddProduct = (product) => {
+    const productExist = cartItems.find((item) => item.id === product.id);
+    if (productExist) {
       setCartItems(
-        cartItems.map((item)=> item.id === product.id ? 
-      {...productExist, quantity:productExist.quantity - 1}: item)
-      )
+        cartItems.map((item) =>
+          item.id === product.id
+            ? { ...productExist, quantity: productExist.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
     }
-  }
+  };
+
+  const handleRemoveProduct = (product) => {
+    const productExist = cartItems.find((item) => item.id === product.id);
+    if (productExist.quantity === 1) {
+      setCartItems(cartItems.filter((item) => item.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id
+            ? { ...productExist, quantity: productExist.quantity - 1 }
+            : item
+        )
+      );
+    }
+  };
+
 
   const handleCartClearance = () => {
     setCartItems([]);
-  }
+  };
 
   return (
     <div className="App">
       <Router>
-        <Header cartItems={cartItems}/>
+        <Header cartItems={cartItems} />
         <Routes>
-          <Route path="/" element={<Products productItems={productItems} handleAddProduct={handleAddProduct}/>}/>
-          <Route path="/cart" element={<Cart cartItems={cartItems} handleAddProduct={handleAddProduct} handleRemoveProduct={handleRemoveProduct} handleCartClearance={handleCartClearance}/>}/>
+          <Route
+            path="/"
+            element={<Products productItems={productItems} handleAddProduct={handleAddProduct} />}
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                cartItems={cartItems}
+                handleAddProduct={handleAddProduct}
+                handleRemoveProduct={handleRemoveProduct}
+                handleCartClearance={handleCartClearance}
+              />
+            }
+          />
+          <Route
+            path="/reviews"
+            element={<Review reviewData={reviewData} />} 
+          />
         </Routes>
       </Router>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,47 @@
-import React from 'react'
-import "./Header.css"
-import { Link } from 'react-router-dom'
-import Search from './Search'
+import React from 'react';
+import "./Header.css";
+import { Link } from 'react-router-dom';
+import Search from './Search';
 
-function Header({cartItems}) {
+function Header({ cartItems }) {
   return (
     <div>
-        <header className='header'>
-            <div>
-                <h1>
-                    <Link to="/" className='logo'>
-                        Shopping Hub
-                    </Link>
-                </h1>
-            </div>
-            <div>
-            <Search/>
-            </div>
-            <div className='links'>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <Link to="/cart" className='cart'>
-                            <i className='fas fa-shopping-cart'/>
-                            <span className='cart-num'>
-                                {cartItems.length === 0 ? "" : cartItems.length}
-                            </span>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </header>
+      <header className='header'>
+        <div>
+          <h1>
+            <Link to="/" className='logo'>
+              Shopping Hub
+            </Link>
+          </h1>
+        </div>
+        <div>
+          <Search />
+        </div>
+        <div className='links'>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to="/reviews">
+                <i className="fas fa-eye" /> Reviews {/* Eye icon */}
+              </Link>
+            </li>
+            <li>
+              <Link to="/cart" className='cart'>
+                <i className='fas fa-shopping-cart' />
+                <span className='cart-num'>
+                  {cartItems.length === 0 ? "" : cartItems.length}
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
